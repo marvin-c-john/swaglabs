8f06f5acaa63f3b75b444836cd130453ca5a27c3
Marvin John <Marvinc.john.03@gmail.com>
2026-08-31 14:07:37 +0200

    refactor: remove unused import and instance

--- tests/login-all-users.spec.ts
+++ tests/login-all-users.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
-import { InventoryPage } from "../pages/InventoryPage";
+
+
 
 const usernames = [
   "standard_user",
@@ -13,13 +14,12 @@ const usernames = [
 
 test("login all users", async ({ page }) => {
   const loginPage = new LoginPage(page);
-  const inventoryPage = new InventoryPage(page);
 
   for (let user of usernames) {
     if (user !== "locked_out_user") {
       await loginPage.login(user);
       await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
-      // await inventoryPage.logout();
+      
     } else {
       continue;
     }
